2780e9cf40a12575dc2ed8341ca292c24ac8e53f
Ayrton Laura <ayrtonlauracaja@gmail.com>
2023-03-10 19:36:14 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,7 @@
+function hola(name) {
+    return "Hola " + name;
+}
+console.log(hola("Pedro"));
+console.log(hola("Juan"));
+console.log(hola(""));
+
---
+++ js-1/sum.js
@@ -0,0 +1,7 @@
+function suma(positivo){
+    
+    return positivo*(positivo+1)/2
+}
+console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)) // 55
+console.log(suma(15)) // 120
